5f406d27426894dcf63bbd5594f59930903ea701
Chester H. Camm <castfire111@gmail.com>
2019-03-30 05:36:37 +0000

    inital sleepy commit

---
+++ src/main.py
@@ -0,0 +1,51 @@
+'''
+Created on 30 Mar 2019
+
+@author: Chester
+'''
+
+import random
+import numpy as np
+from astropy.coordinates.angle_utilities import position_angle
+
+numberOfThrows=10
+boardSize=10 
+#for now do 1D case as the dimensions don't meaningfully interact
+
+Guess= np.zeros(numberOfThrows+1)
+Guess[0]=boardSize/2.0
+
+Throws=np.zeros(numberOfThrows)
+
+Position=random.uniform(0,boardSize)
+exactMatchCounter=0
+
+def findSign(landPos, actualPos):
+    
+    if (landPos<actualPos):
+        return +1
+    elif (landPos>actualPos):
+        return -1
+    else:
+        exactMatchCounter+=1
+        return 0
+    
+def throw(size):
+    return random.uniform(0,size)
+
+def guessDiff(guess,pos):
+    diff=np.zeros(numberOfThrows)
+    
+    for i in range(1,numberOfThrows+1):
+        diff[i-1]=pos-guess[i]
+               
+    return diff
+
+
+
+for i in range(1,numberOfThrows+1):
+    Throws[i-1]=throw(boardSize)
+    
+    Guess[i]=Guess[i-1]+findSign(Throws[i-1], Position)
+    
+print(guessDiff(Guess, Position),Position)
